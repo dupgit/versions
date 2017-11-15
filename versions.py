6e37c8a4cc40fc762f26328ee956d79b4f05e697
dupgit <olivier.delhomme@free.fr>
2017-11-15 21:45:02 +0100

    Version 1.3.0
    New features:
     * A new optional regex field is added to configuration file to ease
       guessing project name and version from a "list" site type.
     * Displays line and column where an error has been detected in a
       bad formatted YAML file.
    
    Bug fix:
     * Avoids a crash when loading a bad formatted YAML file.
    
    Tests:
     * Adds a doctest to ensure bad formatted file detection.

--- versions.py
+++ versions.py
@@ -32,8 +32,8 @@ import doctest
 import re
 
 __author__ = "Olivier Delhomme <olivier.delhomme@free.fr>"
-__date__ = "03.11.2017"
-__version__ = "1.2.1"
+__date__ = "15.11.2017"
+__version__ = "1.3.0"
 
 """
 This program checks projects versions through RSS and Atom feeds and
