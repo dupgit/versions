452d35f15f3182a3dc7748e805f3d998d46a3783
dupgit <olivier.delhomme@free.fr>
2017-11-27 21:38:41 +0100

    Adds versions.yaml real example file to coverage tests.

--- coverage.bash
+++ coverage.bash
@@ -12,3 +12,4 @@ ${COV} run ./versions.py -d -f coverage.yaml
 ${COV} run -a ./versions.py -d -f doesnotexist.yaml
 ${COV} run -a ./versions.py -l -f coverage.yaml
 ${COV} run -a ./versions.py -f bad_formatted.yaml
+${COV} run -a ./versions.py -f versions.yaml
