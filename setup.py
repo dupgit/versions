ad95e8ce651d69a35e060fe4eea2ec16583d39b5
dupgit <olivier.delhomme@free.fr>
2018-01-05 21:25:33 +0100

    Changes published name in pypi to avoid an another versions project.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='versions',  # Required
+    name='program_versions',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
