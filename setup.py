6d8731bdce33b31fd8ed45383be05a6ad98b5022
dupgit <olivier.delhomme@free.fr>
2018-05-22 22:37:31 +0200

    Wrong version number for GPL license that is v3 since first commit.

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
         'Topic :: Software Development :: Version Control',
 
         # Pick your license as you wish
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
