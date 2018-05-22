ca0dc7e862cf0f69027eb7d4a2eb5aa89402220b
dupgit <olivier.delhomme@free.fr>
2018-05-22 21:54:58 +0200

    Version 1.5.1
    * Now byproject site type may have a 'last checked' value to a global
      'entry' key
    
    * License version correction to reflect LICENSE file.

--- versions.py
+++ versions.py
@@ -35,8 +35,8 @@ import yaml
 import operator
 
 __author__ = "Olivier Delhomme <olivier.delhomme@free.fr>"
-__date__ = "04.05.2018"
-__version__ = "1.5.0"
+__date__ = "22.05.2018"
+__version__ = "1.5.1"
 
 """
 This program checks projects versions through RSS and Atom feeds and
