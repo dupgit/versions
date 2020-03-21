5858119607d45160138eb67a905d08664beac4d7
Olivier DELHOMME <olivier.delhomme@free.fr>
2020-03-21 20:31:02 +0100

    Updates coverage script to avoid scrutinizer error.

--- tests/coverage.bash
+++ tests/coverage.bash
@@ -1,17 +1,42 @@
 #!/bin/bash
-# 
+#
 # A basic coverage script
-# Usage : 
+# Usage :
 # coverage.bash COVERAGE-COMMAND
-# Where COVERAGE-COMMAND is the command for 
+# Where COVERAGE-COMMAND is the command for
 # coverage program
+
+set -eu
 
 export COV=$1
 
-${COV} run ../versions/versions.py -d -f coverage.yaml
-${COV} run -a ../versions/versions.py -d -f doesnotexist.yaml
-${COV} run -a ../versions/versions.py -l -f coverage.yaml
-${COV} run -a ../versions/versions.py -f bad_formatted.yaml
-${COV} run -a ../versions/versions.py -f versions.yaml
-${COV} run -a ../versions/versions.py
-${COV} run -a ../versions/versions.py -l
+function header_print {
+
+    echo
+    echo "-------------------------------------------------"
+    echo " Running test $@"
+    echo "-------------------------------------------------"
+    echo
+}
+
+function first_run_test {
+
+    header_print $@
+    ${COV} run ../versions/versions.py $@
+
+}
+
+
+function run_test {
+
+    header_print $@
+    ${COV} run -a ../versions/versions.py $@
+
+}
+
+first_run_test -d -f coverage.yaml
+run_test -d -f doesnotexist.yaml
+run_test -l -f coverage.yaml
+run_test -f bad_formatted.yaml
+run_test -f versions.yaml
+run_test -l -f versions.yaml
